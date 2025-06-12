17bf490de451fcfa033bc2ccc42f0db4e92d60c8
nitin bahuguna <nitinbahuguna1251@gmail.com>
2025-06-12 17:27:19 +0530

    feat: add ContentShare project and associated image; update Projects and ProjectBox components for new project details

--- src/pages/portfolio/Projects.jsx
+++ src/pages/portfolio/Projects.jsx
@@ -1,35 +1,34 @@
-import ProjectBox from '../../pages/portfolio/ProtfolioBox';
-import './portfolio.css'
-import annadata from '../../assets/annadata.png';
-import bgChanger from '../../assets/bg_changer.png';
-import bmi from '../../assets/bmi.png';
-import colorswitch from '../../assets/colorswitch.png';
-import comingsoon from '../../assets/comingsoon.png';
-import rps from '../../assets/rps.png';
-import snakegame from '../../assets/snakegame.png';
-import ttt from '../../assets/ttt.png';
-import devdate from '../../assets/devdate.jpg';
-
-
+import ProjectBox from "../../pages/portfolio/ProtfolioBox";
+import "./portfolio.css";
+import annadata from "../../assets/annadata.png";
+import bgChanger from "../../assets/bg_changer.png";
+import bmi from "../../assets/bmi.png";
+import colorswitch from "../../assets/colorswitch.png";
+import comingsoon from "../../assets/comingsoon.png";
+import rps from "../../assets/rps.png";
+import snakegame from "../../assets/snakegame.png";
+import ttt from "../../assets/ttt.png";
+import devdate from "../../assets/devdate.jpg";
+import share from "../../assets/share.png";
 
 const Projects = () => {
   return (
-    <section className='section'>
-
+    <section className="section">
       <h2 className="title">
         MY <span>PROJECTS</span>
       </h2>
 
-      <p className='para'>
+      <p className="para">
         {` Here are some of the projects I've worked on. Some of them are still in development, others are completed.
         Click on the project name for more information or to see the live version.
         If you want to contribute to any of these projects, feel free to reach out to me.`}
       </p>
 
-      <div className='project'>
+      <div className="project">
         <ProjectBox projectPhoto={annadata} projectName="Annadata" />
-        <ProjectBox projectPhoto={snakegame} projectName="SnakeGame" />
         <ProjectBox projectPhoto={devdate} projectName="DevDate" />
+        <ProjectBox projectPhoto={share} projectName="ContentShare" />
+        <ProjectBox projectPhoto={snakegame} projectName="SnakeGame" />
         <ProjectBox projectPhoto={bmi} projectName="BMI" />
         <ProjectBox projectPhoto={ttt} projectName="TicTacToe" />
         <ProjectBox projectPhoto={bgChanger} projectName="BgChanger" />
@@ -37,9 +36,8 @@ const Projects = () => {
         <ProjectBox projectPhoto={rps} projectName="RockPaperScissor" />
         <ProjectBox projectPhoto={comingsoon} projectName="Comingsoon" />
       </div>
-
     </section>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
--- src/pages/portfolio/ProtfolioBox.jsx
+++ src/pages/portfolio/ProtfolioBox.jsx
@@ -1,82 +1,97 @@
-
 import { FaGithub } from "react-icons/fa";
 import { CgFileDocument } from "react-icons/cg";
-import PropTypes from 'prop-types';
-
+import PropTypes from "prop-types";
 
 const ProjectBox = ({ projectPhoto, projectName }) => {
   const desc = {
-    AnnadataDesc: "This is an Online Food Delivery platform designed to provide a seamless and efficient way for users to browse menus and place orders.",
+    AnnadataDesc:
+      "This is an Online Food Delivery platform designed to provide a seamless and efficient way for users to browse menus and place orders.",
     AnnadataGithub: "https://github.com/nitin-04/Annadata",
-    AnnadataWebsite: "https://drive.google.com/file/d/1XzUqj_syzqxTD083xRY7mSHtS4kSStd2/view",
+    AnnadataWebsite:
+      "https://drive.google.com/file/d/1XzUqj_syzqxTD083xRY7mSHtS4kSStd2/view",
 
-    SnakeGameDesc: "A simple Snake Game where the snake grows by eating food, with gameplay ending upon collision with the walls or itself.",
-    SnakeGameGithub: "https://github.com/nitin-04/snakeGame",
-    SnakeGameWebsite: "https://snake-game-sg.vercel.app/",
-
-    DevDateDesc: "Developed Developer Connect, a MERN stack web app enabling developers to network, send/reject connection requests, and collaborate seamlessly.",
+    DevDateDesc:
+      "Developed Developer Connect, a MERN stack web app enabling developers to network, send/reject connection requests, and collaborate seamlessly.",
     DevDateGithub: "https://github.com/nitin-04/DevDate-web",
     DevDateWebsite: "",
 
-    BMIDesc: "BMI Calculator: Enter height and weight to calculate BMI and get categorized results: Underweight, Normal, Overweight, or Obese.",
+    ContentShareDesc:
+      "Content Share is a platform for sharing and discovering content, allowing users to upload files, Ppt, Video and Pdf.",
+    ContentShareGithub: "https://github.com/nitin-04/ContentShare-Web",
+    ContentShareWebsite: "https://content-share-web.vercel.app/",
+
+    SnakeGameDesc:
+      "A simple Snake Game where the snake grows by eating food, with gameplay ending upon collision with the walls or itself.",
+    SnakeGameGithub: "https://github.com/nitin-04/snakeGame",
+    SnakeGameWebsite: "https://snake-game-sg.vercel.app/",
+    BMIDesc:
+      "BMI Calculator: Enter height and weight to calculate BMI and get categorized results: Underweight, Normal, Overweight, or Obese.",
     BMIGithub: "https://github.com/nitin-04/bmi",
     BMIWebsite: "https://bmi-dusky-omega.vercel.app",
 
-    BgChangerDesc: "A background changer built with JavaScript featuring Start and Stop buttons. Press Start to change the background color every 2 seconds and Stop to pause the effect.",
+    BgChangerDesc:
+      "A background changer built with JavaScript featuring Start and Stop buttons. Press Start to change the background color every 2 seconds and Stop to pause the effect.",
     BgChangerGithub: "https://github.com/nitin-04/Unlimited-background-colors",
     BgChangerWebsite: "https://unlimited-background-colors.vercel.app/",
 
-    ColorSwitchDesc: "A Color Switcher built with JavaScript that changes the background based on the selected color, providing a simple and interactive user experience.",
+    ColorSwitchDesc:
+      "A Color Switcher built with JavaScript that changes the background based on the selected color, providing a simple and interactive user experience.",
     ColorSwitchGithub: "https://github.com/nitin-04/color-switch  ",
     ColorSwitchWebsite: "https://color-switch-five.vercel.app/",
 
-    TicTacToeDesc: "A dynamic Tic Tac Toe game using JavaScript, featuring a 3x3 grid with real-time win, loss, and draw detection.",
+    TicTacToeDesc:
+      "A dynamic Tic Tac Toe game using JavaScript, featuring a 3x3 grid with real-time win, loss, and draw detection.",
     TicTacToeGithub: "https://github.com/nitin-04/TicTacToe",
     TicTacToeWebsite: "https://tic-tac-toe-eight-lovat.vercel.app/",
 
-    RockPaperScissorDesc: "A single-player Rock-Paper-Scissors game where the player competes against the computer. The computer makes random choices.",
+    RockPaperScissorDesc:
+      "A single-player Rock-Paper-Scissors game where the player competes against the computer. The computer makes random choices.",
     RockPaperScissorGithub: "https://github.com/nitin-04/RPS",
     RockPaperScissorWebsite: "https://rps-bice-five.vercel.app/",
 
     // CommingsoonDesc: "",
     // ComingsoonGithub: "",
     // ComingsoonWebsite: "",
+  };
 
-  }
-
-  let show = '';
-  if (desc[projectName + 'Github'] === "") {
+  let show = "";
+  if (desc[projectName + "Github"] === "") {
     show = "none";
   }
 
   return (
-    <div className='projectBox'>
-      <img className='projectPhoto' src={projectPhoto} alt="Project display" />
+    <div className="projectBox">
+      <img className="projectPhoto" src={projectPhoto} alt="Project display" />
       <div>
         <br />
         <h3 className="project_name">{projectName}</h3>
         <br />
-        <div className="projectDesc">
-          {desc[projectName + 'Desc']}
-        </div>
+        <div className="projectDesc">{desc[projectName + "Desc"]}</div>
         <br />
 
-        <a style={{ display: show }} href={desc[projectName + 'Github']} target='_blank'>
-          <button className='projectbtn'><FaGithub /> Github</button>
+        <a
+          style={{ display: show }}
+          href={desc[projectName + "Github"]}
+          target="_blank"
+        >
+          <button className="projectbtn">
+            <FaGithub /> Github
+          </button>
         </a>
 
-        <a href={desc[projectName + 'Website']} target='_blank'>
-          <button className='projectbtn'><CgFileDocument /> Demo</button>
+        <a href={desc[projectName + "Website"]} target="_blank">
+          <button className="projectbtn">
+            <CgFileDocument /> Demo
+          </button>
         </a>
       </div>
     </div>
-  )
-}
+  );
+};
 
 ProjectBox.propTypes = {
   projectPhoto: PropTypes.string.isRequired,
-  projectName: PropTypes.string.isRequired
+  projectName: PropTypes.string.isRequired,
 };
-
 
 export default ProjectBox;
